23668c84a4813a9ce757ec585984587fd720f78f
tarun chaudhary <chaudharytarun0001@gmail.com>
2022-05-25 20:46:29 +0530

    complete-signin-screen

--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -3,6 +3,8 @@ import { createContext, useReducer } from "react";
 export const Store = createContext();
 
 const initialState = {
+    userInfo : localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null,
+    
     cart: {
         cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
     },
@@ -22,7 +24,7 @@ function reducer(state,action){
              )
              : [...state.cart.cartItems, newItem];
              localStorage.setItem('cartItems', JSON.stringify(cartItems));
-      return { ...state, cart: { ...state.cart, cartItems } };
+        return { ...state, cart: { ...state.cart, cartItems } };
            
         case 'CART_REMOVE_ITEM' : {
             const cartItems = state.cart.cartItems.filter((item) =>
@@ -31,6 +33,13 @@ function reducer(state,action){
             localStorage.setItem('cartItems', JSON.stringify(cartItems));
             return { ...state, cart: { ...state.cart, cartItems}};
         }
+
+        case 'USER_SIGNIN':
+            return {...state, userInfo: action.payload};
+        
+        case 'USER_SIGNOUT':
+            return {...state, userInfo: null}    
+
         default:
             return state;    
     }
